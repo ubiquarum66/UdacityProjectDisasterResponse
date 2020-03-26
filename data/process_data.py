abb76730c99446ea1afe236a7a9a8be1599eed17
Thomas <sabine_thomas_es@t-online.de>
2020-03-26 09:34:07 +0100

    feat: Introduced csv reading and setting up Pandas DataFrame

--- data/process_data.py
+++ data/process_data.py
@@ -1,9 +1,29 @@
 import sys
-
+# import libraries
+import re
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    # load the csv file containing all the tweets , in english, original, and with ID given ,
+    # also there is a column genre, given the way the tweets were received
+    # load messages dataset
+    print("#-----------try loading data")
+    messages = pd.read_csv(messages_filepath, categories_filepath,encoding='latin-1')
+    print(messages.head(2))
+    # load the csv file containing all labesls assigned to the tweets externally (expert knowledge)
+    # identified by id and coded by a concat string of ever the same sequence, with bin info added as suffix to the toüpic name
+    # e.g. blue-0 = not blue,  blue-1 = is blue
+    # load categories dataset
+    categories =  pd.read_csv(messages_filepath, categories_filepath, encoding='latin-1')
+    print(categories.head(2))
+    #merge data according to ID .....
+    df = messages.merge(categories, on='id')
+    print(df.head(2))
+    print("#------------data loaded successfully")
+    return df
+    
 
 def clean_data(df):
     pass
